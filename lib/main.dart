13a6ac6b001e6b1c1636639304cab0ae347c321e
Mariam El Sheikh <81959358+mariam123570@users.noreply.github.com>
2023-12-28 00:50:43 +0200

    home screen design

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:nature_app/modules/homepage/home_page.dart';
 import 'package:nature_app/modules/splash/splach_screen.dart';
 import 'package:nature_app/services/app/app_services.dart';
 
@@ -18,7 +19,7 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.green),
         useMaterial3: true,
       ),
-      home: SplashPage(),
+      home: HomePage(),
     );
   }
 }
